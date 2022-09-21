6ffa55a57810de05c50f57258a5317c51146894e
cwray2 <cwray2@crimson.ua.edu>
2022-09-20 21:02:20 -0500

    Add project files.

---
+++ WebApplication1/wwwroot/script.js
@@ -0,0 +1,123 @@
+﻿var len;
+var results = '';
+
+function apiSearch() {
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1a5e253db99a4b71b23b270302ab8b89");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            results = "";
+            for (i = 0; i < len; i++) {
+                results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+            }
+
+            $('#searchResults').html(results);
+            $('#searchResults').dialog({
+                title: 'Peak Results for "' + $("#query").val() + '"',
+                width: '100%',
+                height: ($(window).height() - 75),
+                close: close,
+                modal: true,
+                show: {
+                    effect: "slide",
+                    duration: 1500
+                },
+                hide: {
+                    effect: "slide",
+                    duration: 1500
+                }
+
+            });
+        })
+        .fail(function () {
+            alert("error");
+        });
+}
+
+function initSearch() {
+    //var searchID = document.getElementById("query").value;
+    //console.log(searchID);
+
+    apiSearch();
+}
+
+function changeBackground() {
+    if (document.body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60')") {
+
+        document.body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60')";
+
+    } else if (document.body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60')") {
+
+        document.body.style.backgroundImage = "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60')";
+
+    }
+
+}
+
+function timeClick() {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
+
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+
+    $('#time').dialog({
+        width: 300,
+        height: 150,
+        show: {
+            effect: "slide",
+            duration: 1500
+        },
+        hide: {
+            effect: "slide",
+            duration: 1500
+        }
+    });
+    startTime();
+}
+
+function initRando() {
+    apiSearch2();
+}
+
+function apiSearch2() {
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1a5e253db99a4b71b23b270302ab8b89");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            window.open(data.webPages.value[0].url);
+        })
+}
